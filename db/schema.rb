0c981d5bfe58b9348ece425111ef03f1a5b1ba0b
Burns Severson <contestburns@gmail.com>
2017-02-26 17:22:50 +0000

    Added Plans Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220171954) do
+ActiveRecord::Schema.define(version: 20170226171547) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
